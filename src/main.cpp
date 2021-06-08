8576f517d8f1f22136b53a6e05ffaadd31a43f64
Tomasz Tarnowski <tomasz.tarnowski@thirdbridge.com>
2021-06-08 22:09:48 +0100

    Day40: Controlling pump with transistor from digital pin D1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(2000);
+
+  pinMode(D1, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(D1, HIGH);
+  delay(1000);
+  digitalWrite(D1, LOW);
+  delay(1000);
 }
